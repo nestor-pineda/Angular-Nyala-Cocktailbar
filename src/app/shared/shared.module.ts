d59e73399a02f16dc8b096272749cd8ead68438c
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-12-23 09:08:35 +0100

    List shared component created and implemented int coctails page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent],
   imports: [CommonModule, HttpClientModule],
+  exports: [ListComponent],
 })
 export class SharedModule {}
